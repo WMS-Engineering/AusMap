8deceaba6db9c8e725be3a932899c9c9dd9a5dc2
Alexandra Balicki <alexandra.balicki@gmail.com>
2024-09-18 12:19:16 +1000

    Update QLR file

--- utils/constants.py
+++ utils/constants.py
@@ -1,2 +1,6 @@
-ABOUT_FILE_URL = 'http://watermodelling.com.au/tools/ausmap/about/'
-CONFIG_FILE_URL = 'https://gist.githubusercontent.com/WaterModellingSolutions/0702699f4000e86689b7382b22fb4482/raw/d70309dff920507a1c9e7be4170d0e944d4a55ed/AusMapLayers.qlr'
+# TODO: Where do we save the QLR file when it's ready?
+
+ABOUT_FILE_URL = 'https://wmseng.com.au/' # TODO: Add tool description on WMS website
+# CONFIG_FILE_URL = 'https://gist.githubusercontent.com/WaterModellingSolutions/0702699f4000e86689b7382b22fb4482/raw/d70309dff920507a1c9e7be4170d0e944d4a55ed/AusMapLayers.qlr' # Original
+
+CONFIG_FILE_URL = 'C:/Users/alexandra.balicki/AppData/Roaming/QGIS/QGIS3/profiles/default/python/plugins/AusMap/ready_for_review.qlr' # New
